3abcb6979169edad01544a25808e608d1a0cbb84
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2020-08-06 16:34:51 -0500

    add domain

--- fleximatic/models/sale_order_line/sale_order_line.py
+++ fleximatic/models/sale_order_line/sale_order_line.py
@@ -6,6 +6,4 @@ class fleximaticsaleorderline(models.Model):
     _inherit = 'sale.order.line'
 
     
-    pricelist_id = fields.Many2one('product.pricelist',string='Pricelist'
-    #,domain="[('item_ids.product_tmpl_id', '=', product_id.product_tmpl_id)]"
-    )
+    pricelist_id = fields.Many2one('product.pricelist',string='Pricelist',domain="[('item_ids.product_tmpl_id', '=', product_id.product_tmpl_id.id)]")
