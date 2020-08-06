77a4ba2a41745eaba5c91782a3338da386579a07
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2020-08-06 16:08:42 -0500

    add domain

--- fleximatic/models/sale_order_line/sale_order_line.py
+++ fleximatic/models/sale_order_line/sale_order_line.py
@@ -5,5 +5,5 @@ from odoo import models,fields, api
 class fleximaticsaleorderline(models.Model):
     _inherit = 'sale.order.line'
 
-    #,domain="[('item_ids.product_tmpl_id', '=', product_id.product_tmpl_id)"
-    pricelist_id = fields.Many2one('product.pricelist',string='Pricelist')
+    
+    pricelist_id = fields.Many2one('product.pricelist',string='Pricelist',domain="[('item_ids.product_tmpl_id', '=', product_id.product_tmpl_id)]")
