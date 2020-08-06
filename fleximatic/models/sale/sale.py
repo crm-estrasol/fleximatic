951242195cdda5f73db93a962765d2044f2d2a23
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2020-08-06 15:59:20 -0500

    agregnado campo en sale order line3

--- fleximatic/models/sale/sale.py
+++ fleximatic/models/sale/sale.py
@@ -1,11 +1,2 @@
-# -*- coding: utf-8 -*-
-from odoo import models,fields, api
 
-
-class fleximaticsaleorderline(models.Model):
-    
-    _inherit = sale.order.line
-
-    #,domain="[('item_ids.product_tmpl_id', '=', product_id.product_tmpl_id)"
-    pricelist_id = fields.Many2one('product.pricelist',string='Pricelist')
     
