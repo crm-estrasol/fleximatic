d0c7e8430faaa9acce95c5e6bb1be8060524ae72
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2020-08-07 12:32:56 -0500

    rescribiendo metodos

--- fleximatic/models/sale_order_line/sale_order_line.py
+++ fleximatic/models/sale_order_line/sale_order_line.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
-from odoo import models,fields, api
+from odoo import api, fields, models, SUPERUSER_ID, _
 
+from datetime import datetime, timedelta
+from functools import partial
+from itertools import groupby
+
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.tools.misc import formatLang, get_lang
+from odoo.osv import expression
+from odoo.tools import float_is_zero, float_compare
 
 class fleximaticsaleorderline(models.Model):
     _inherit = 'sale.order.line'
