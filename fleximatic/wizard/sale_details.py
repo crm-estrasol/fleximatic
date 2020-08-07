07c179797df12015ed50a2af67e97d879cce1edb
Kevin-lCampo-Estrasol <kcampo@estrasol.com.mx>
2020-08-07 10:30:53 -0500

    asd

--- fleximatic/wizard/sale_details.py
+++ fleximatic/wizard/sale_details.py
@@ -15,36 +15,17 @@ class ItemPricelist(models.TransientModel):
     pricelist_id =   fields.Many2one('product.pricelist', string='Pricelist')
     pricelist_avaible =  fields.Many2many(comodel_name='product.pricelist.item', relation='table_many_pricelist_item', column1='product_id', column2='',string="Tarifas disponibles")
     date_order =  fields.Datetime(string='Date', readonly=True, default=fields.Datetime.now)
-    
-    """
-    def generate_report(self):    
-        for prod in self.sale.order_line:
-            goal = []
-            if self.projects:
-                goal.append( True if prod.project in self.projects  else False )
-            if self.ubications:
-                goal.append( True if prod.ubication  in self.ubications  else False )
-            if self.brand:
-                goal.append( True if prod.product_id.brand in self.brand  else False )
-            if self.partner:
-                sellers = []
-                for seller in prod.product_id.seller_ids:
-                        sellers.append(seller.name.id)     
-                goal.append( True if  any(i in sellers for i in self.partner.ids) else False )
-            
-            prod.discount = self.discount  if all(goal if goal else False)  else prod.discount
-        return self.sale
-   """
+  
     def generate_apply(self):
         products = self.sale.order_line.filtered(lambda x: x.product_id == self.product_id.id)
         _logger.info(str(products))
         for prod in products:
-            prod.pricelist_id = self.pricelist_id.id
+            prod.pricelist_id = self.pricelist_id
         return self.sale
     def generate_apply_next(self):
         products = self.sale.order_line.filtered(lambda x: x.product_id == self.product_id.id)
         for prod in products:
-            prod.pricelist_id = self.pricelist_id.id
+            prod.pricelist_id = self.pricelist_id
     @api.onchange('sale','product_id')
     def on_change_sale(self):
         self.pricelist_id = False
