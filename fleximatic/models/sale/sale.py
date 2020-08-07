e0a6791f3db5b6a398d95aa63bc8225a4336aff6
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2020-08-07 15:59:35 -0500

    haciendo cambios

--- fleximatic/models/sale/sale.py
+++ fleximatic/models/sale/sale.py
@@ -1,2 +1,21 @@
 
-    
+    # -*- coding: utf-8 -*-
+from odoo import api, fields, models, SUPERUSER_ID, _
+
+
+class fleximaticsale(models.Model):
+    _inherit = 'sale.order'
+
+
+    points = fields.Float('Points',digits=(32, 2),compute='_compute_total_points',store=True)
+
+    @api.depends('order_line')
+    def _compute_total_points(self):
+        puntos = 0
+        for sale in self:
+            if sale.order_line:
+                for line in sale.order_line:
+                    puntos += line.price_subtotal *(1/100)
+            if puntos > 0:
+                sale.points = puntos
+                        
