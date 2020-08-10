8da75806af1f63726bb51e4336c8a51ab8898039
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2020-08-10 15:18:25 -0500

    agregando cambios

--- fleximatic/models/product/product.py
+++ fleximatic/models/product/product.py
@@ -5,4 +5,8 @@ from odoo import api, fields, models, SUPERUSER_ID, _
 class fleximaticproduct(models.Model):
     
     _inherit = 'product.template'
+
+    vender_puntos = fields.Boolean('Puede venderse con puntos')
+    puntos_venta = fields.Float('Puntos para venta')
+    puntos_genera = fields.Float('Cantidad de puntos que genera(%)')
     
--- fleximatic/models/sale/sale.py
+++ fleximatic/models/sale/sale.py
@@ -21,7 +21,8 @@ class fleximaticsale(models.Model):
         for sale in self:
             if sale.order_line:
                 for line in sale.order_line:
-                    puntos += line.price_subtotal *(1/100)
+                    if line.product_id.vender_puntos == 'True':
+                        puntos += line.price_subtotal *(line.product_id.puntos_genera/100)
             sale.points = puntos
 
     @api.depends('x_credit_after_sale','x_credit','amount_total')
