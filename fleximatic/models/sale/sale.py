219bd5f043e92f72001a9297dffa2bf8d0518942
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2020-08-10 12:11:20 -0500

    agregando campos no referenciados

--- fleximatic/models/sale/sale.py
+++ fleximatic/models/sale/sale.py
@@ -6,7 +6,13 @@ from odoo import api, fields, models, SUPERUSER_ID, _
 class fleximaticsale(models.Model):
     _inherit = 'sale.order'
 
-
+    x_approve = fields.Selection([
+        ('to_approve','To Approve'),
+        ('approved','Approved'),
+        ('not_approve','Not Approve')],
+        string='Approve')
+    x_credit = fields.Monetary('related=partner_id.x_credit','Available Credit')
+    x_credit_after_sale = fields.Monetary('Credit After Sale',compute = 'compute_credit_after_sale')
     points = fields.Float('Points',digits=(32, 2),compute='_compute_total_points',store=True)
 
     @api.depends('order_line')
@@ -17,4 +23,8 @@ class fleximaticsale(models.Model):
                 for line in sale.order_line:
                     puntos += line.price_subtotal *(1/100)
             sale.points = puntos
-                        
+
+    @api.depends('x_credit_after_sale','x_credit','amount_total')
+    def compute_credit_after_sale(self):
+        for record in self:
+            record['x_credit_after_sale'] = record.x_credit - record.amount_total   
