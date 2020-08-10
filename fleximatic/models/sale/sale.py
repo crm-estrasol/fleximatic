7e5d096108e9c38d83fca7445383f335d28a40f3
Kevin-lCampo-Estrasol <kcampo@estrasol.com.mx>
2020-08-10 12:57:47 -0500

    a

--- fleximatic/models/sale/sale.py
+++ fleximatic/models/sale/sale.py
@@ -1,7 +1,14 @@
-
-    # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from odoo import api, fields, models, SUPERUSER_ID, _
 
+from datetime import datetime, timedelta
+from functools import partial
+from itertools import groupby
+
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.tools.misc import formatLang, get_lang
+from odoo.osv import expression
+from odoo.tools import float_is_zero, float_compare
 
 class fleximaticsale(models.Model):
     _inherit = 'sale.order'
@@ -14,7 +21,21 @@ class fleximaticsale(models.Model):
     x_credit = fields.Monetary(related='partner_id.x_credit',string='Available Credit')
     x_credit_after_sale = fields.Monetary('Credit After Sale',compute = 'compute_credit_after_sale')
     points = fields.Float('Points',digits=(32, 2),compute='_compute_total_points',store=True)
+    def show_pricelistAvaible(self):
+        if self.order_line:   
+            view_id = self.env.ref('fleximatic.view_sale_pricelist_wizard').id
+            view = {
+                'name': ('Descuento'),
+                'view_type': 'form',
+                'view_mode': 'form',
+                'res_model': 'fleximatic.sale.pricelist.wizard',
+                'views':  [(view_id,'form')],
+                'type': 'ir.actions.act_window',
+                'target': 'new',
+                'context':{'default_sale':self.id,'default_date_order':self.date_order,'default_product_id':self.order_line[0].product_id.id},
 
+                }
+        return view 
     @api.depends('order_line')
     def _compute_total_points(self):
         puntos = 0
@@ -28,3 +49,7 @@ class fleximaticsale(models.Model):
     def compute_credit_after_sale(self):
         for record in self:
             record['x_credit_after_sale'] = record.x_credit - record.amount_total   
+
+    
+
+    
