d873481f94cb4360fe25272fc2495b396560c470
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2020-08-10 18:04:16 -0500

    cambios

--- fleximatic/wizard/promotional_products.py
+++ fleximatic/wizard/promotional_products.py
@@ -11,48 +11,24 @@ class productPromotional(models.TransientModel):
 
     name = fields.Char('name')
     sale_id = fields.Many2one('sale.order',string='Sale')
-    points = fields.Float(string='Points',related='sale_id.points')
-    points_to_sale = fields.Float(string='Points to sale', compute = 'total_points_to_sale')
+    points = fields.Float(string='Points')
+    points_to_sale = fields.Float(string='Points to sale')
     promotional_line = fields.One2many('product.promotional.line','promotional_id')
 
-    @api.depends('promotional_line')
-    def total_points_to_sale(self):
-        for value in self:
-            total = 0.0
-            if value.promotional_line:
-                for line in value.promotional_line:
-                    total += line.total
-            value.points_to_sale = total
+    
+   
 
-    def agregar(self):
-        if self.points_to_sale > points:
-            raise ValidationError(('Error ! Insufficient points to add product(s)'))
-        else:
-            for line in promotional_line:
-                self.sale_id.order_line.write(0,0,{
-                    'product_id':line.product_template_id.product_variant_id.id,
-                    'product_template_id':line.product_template_id.id,
-                    'is_promotional':True,
-                    'product_uom_qty':line.qty,
-                    'product_uom':line.uom_id,
-                    'price_unit':0.01,
-                    'tax_id':False,
-                    'discount':0.00
-                })
-            return {'type': 'ir.actions.act_window_close'}
-
-    def cancelar(self):
-        return {'type': 'ir.actions.act_window_close'}
+    
 
 
 class productPromotionalLine(models.TransientModel):
     _name = 'product.promotional.line'
 
-    product_template_id = fields.Many2one('product.template', string='Product', domain=[('sale_ok', '=', True),('vender_puntos','=',True),('puntos_venta','<=',promotional_id.points)])
+    product_template_id = fields.Many2one('product.template', string='Product')
     qty  = fields.Integer('Quantity')
-    price_points = fields.Float('Points for sale',related='product_template_id.puntos_venta')
-    uom_id =fields.Many2one('uom.uom',stirng='UoM',related='product_template_id.uom_id')
-    total = fields.Float('Total',compute='_compute_total_points')
+    price_points = fields.Float('Points for sale')
+    uom_id =fields.Many2one('uom.uom',stirng='UoM')
+    total = fields.Float('Total')
     promotional_id = fields.Many2one('product.promotional')
 
     @api.depends('product_template_id','qty','price_points')
