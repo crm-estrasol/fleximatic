66b0278354e71e165421be73d2ea497f3a851323
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2020-08-10 17:34:21 -0500

    cambios

--- fleximatic/models/sale/sale.py
+++ fleximatic/models/sale/sale.py
@@ -12,17 +12,19 @@ class fleximaticsale(models.Model):
         string='Approve')
     x_credit = fields.Monetary(related='partner_id.x_credit',string='Available Credit')
     x_credit_after_sale = fields.Monetary('Credit After Sale',compute = 'compute_credit_after_sale')
-    points = fields.Float('Points',digits=(32, 2),compute='_compute_total_points',store=True)
+    points = fields.Float('Points',digits=(32, 2),
+    #compute='_compute_total_points',
+    store=True)
 
-    @api.depends('order_line')
-    def _compute_total_points(self):
-        for sale in self:
-            puntos = 0.0
-            if sale.order_line:
-                for line in sale.order_line:
-                    if line.product_id.is_promotional == False:
-                        puntos += line.price_subtotal *(line.product_id.puntos_genera/100)
-            sale.points = puntos
+    #@api.depends('order_line')
+    #def _compute_total_points(self):
+    #    for sale in self:
+    #        puntos = 0.0
+    #        if sale.order_line:
+    #            for line in sale.order_line:
+    #                if line.product_id.is_promotional == False:
+    #                    puntos += line.price_subtotal *(line.product_id.puntos_genera/100)
+    #        sale.points = puntos
 
     @api.depends('x_credit_after_sale','x_credit','amount_total')
     def compute_credit_after_sale(self):
