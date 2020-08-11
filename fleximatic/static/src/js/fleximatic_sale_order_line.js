250cb9549f65b3d4f314d7b0585979d42a03daab
Kevin-lCampo-Estrasol <kcampo@estrasol.com.mx>
2020-08-11 11:22:07 -0500

    s

--- fleximatic/static/src/js/fleximatic_sale_order_line.js
+++ fleximatic/static/src/js/fleximatic_sale_order_line.js
@@ -27,7 +27,7 @@ odoo.define('tomcat.tomcat_sale_order_line', function (require) {
                         
                     } else {
                         $cell.removeClass('o_invisible_modifier');
-                        return $cell.addClass('invisible');
+                        return $cell.addClass('hidden');
                     }   
 
                 }
