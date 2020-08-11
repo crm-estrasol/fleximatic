ed79ff5f108172cec183cb8502039f943ca3db88
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2020-08-11 18:18:20 -0500

    cambios

--- fleximatic/models/sale_order_line/sale_order_line.py
+++ fleximatic/models/sale_order_line/sale_order_line.py
@@ -15,7 +15,7 @@ class fleximaticsaleorderline(models.Model):
 
     
     pricelist_id = fields.Many2one('product.pricelist',string='Pricelist',
-        domain="[('item_ids.product_tmpl_id', '=', product_template_id),'|',('item_ids.product_id','=',product_id)]")
+        domain="['|',('item_ids.product_tmpl_id', '=', product_template_id),'|',('item_ids.product_id','=',product_id)]")
     is_promotional = fields.Boolean('Promotional')
 
     @api.onchange('product_id', 'price_unit', 'product_uom', 'product_uom_qty', 'tax_id','pricelist_id')
