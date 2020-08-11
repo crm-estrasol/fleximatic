bd67f6f0a3dfbaf5a41b187c4c0176931ca6bc15
Kevin-lCampo-Estrasol <kcampo@estrasol.com.mx>
2020-08-11 10:34:46 -0500

    s

--- fleximatic/static/src/js/fleximatic_sale_order_line.js
+++ fleximatic/static/src/js/fleximatic_sale_order_line.js
@@ -17,14 +17,12 @@ odoo.define('tomcat.tomcat_sale_order_line', function (require) {
              */
             _renderBodyCell: function (record, node, index, options) {
                 var $cell = this._super.apply(this, arguments);
-               
-                var isSection = record.data.display_type === 'line_section';
-                var isNote = record.data.display_type === 'line_note';
-
-                if (isSection || isNote) {
+                var isPromotional = record.data.is_promotional === true;
+                console.log(isPromotional)
+                if(isPromotional){
                     if (node.attrs.widget === "handle") {
                         return $cell;
-                    } else if (node.attrs.name === "name") {
+                    } else if (node.attrs.name === "name" ||  node.attrs.name === "product_id" ||  node.attrs.name === "product_uom_qty" |  node.attrs.name === "price_subtotal") {
                         var nbrColumns = this._getNumberOfCols();
                         if (this.handleField) {
                             nbrColumns--;
@@ -33,13 +31,14 @@ odoo.define('tomcat.tomcat_sale_order_line', function (require) {
                             nbrColumns--;
                         }
                         $cell.attr('colspan', nbrColumns);
-                        //$cell.removeClass('oe_edit_only');
-                        //$cell.addClass('oe_read_only');
+                        
                     } else {
                         $cell.removeClass('o_invisible_modifier');
                         return $cell.addClass('o_hidden');
-                    }
+                    }   
+
                 }
+               
 
                 return $cell;
             },
@@ -51,26 +50,13 @@ odoo.define('tomcat.tomcat_sale_order_line', function (require) {
             _renderRow: function (record, index) {
                 var $row = this._super.apply(this, arguments);
 
-                if (record.data.display_type) {
-                    $row.addClass('o_is_' + record.data.display_type);
+                if (record.data.is_promotional) {
                     $($row).find('.o_list_record_remove').remove();
                 }
                 
-                $($row).find('.o_list_record_remove');
                 return $row;
             },
-            /**
-             * We want to add .o_section_and_note_list_view on the table to have stronger CSS.
-             *
-             * @override
-             * @private
-             */
-            _renderView: function () {
-                var self = this;
-                return this._super.apply(this, arguments).then(function () {
-                    self.$('.o_list_table').addClass('o_section_and_note_list_view');
-                });
-            }
+        
     });
     
    
