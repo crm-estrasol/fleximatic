e8dd9588a6177748f7310aec400b2ea5aa809284
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2020-08-13 12:12:19 -0500

    cambios

--- fleximatic/__manifest__.py
+++ fleximatic/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','product','sale','sale_margin'],
+    'depends': ['base','product','mrp','sale','stock','sale_margin','sale_management','contacts'],
 
     # always loaded
     'data': [
