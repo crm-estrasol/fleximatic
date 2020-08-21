b855c247cabd74c828a040cea1d4ab2fe54cd7c3
ArturoJassoEstrasol <ajasso@estrasol.com.mx>
2020-08-21 09:49:23 -0500

    agregando

--- fleximatic/models/stock/stock_picking.py
+++ fleximatic/models/stock/stock_picking.py
@@ -16,9 +16,10 @@ class fleximaticstock(models.Model):
         ],string='Approve freight')
     x_currency_id = fields.Many2one('res.currency',string='Currency')
     x_freight = fields.Float('freight %', digits=(32, 2), compute='compute_total_porcent', store=True,)
-    x_freight_cost = fields.Monetary('Freight cost',related='x_logistics.amount_total')
+    
     x_logistics = fields.Many2one('purchase.order',string='Logistics purchase')
     x_total = fields.Monetary('Sale amount')
+    x_freight_cost = fields.Monetary('Freight cost',related='x_logistics.amount_total')
 
     @api.depends('x_freight','x_total','x_freight_cost')
     def compute_total_porcent(self):
