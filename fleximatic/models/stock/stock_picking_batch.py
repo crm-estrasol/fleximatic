9e0925f3197596ab998776f488e55930884f533a
Kevin-lCampo-Estrasol <kcampo@estrasol.com.mx>
2020-08-31 15:12:06 -0500

    a

--- fleximatic/models/stock/stock_picking_batch.py
+++ fleximatic/models/stock/stock_picking_batch.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
-
-from odoo import api, fields, models, _
-from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationError, Warning
+from odoo import models, fields, api ,_
 import logging
 _logger = logging.getLogger(__name__)
-
+from odoo.tools.misc import clean_context
+from odoo.exceptions import UserError
+import sys
 class fleximaticstockbatch(models.Model):
     _inherit = 'stock.picking.batch'
     x_purchase = fields.Many2one('purchase.order',string='Logistics purchase')
     total_sales = fields.Float('Total ventas', compute='_compute_total_sales')
+    """
     @api.onchange('picking_ids')
     def _onchange_pickings(self):
         if len(self.picking_ids) == len(self.picking_ids.filtered(lambda x:self.x_purchase == x.x_logistics ) ) :
             pass
         else:
-            pass
-
+            raise UserError(_("Exediste el tamaño permitido (1mb/10000) para la imagen ."))
+    """  
+    @api.onchange('x_purchase')  
+    def _onchange_purchase(self):    
             
