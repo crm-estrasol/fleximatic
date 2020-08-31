d771cb799bd2aeebd31f035c5d3d0d667b12e5c3
Kevin-lCampo-Estrasol <kcampo@estrasol.com.mx>
2020-08-31 15:03:20 -0500

    asd

--- fleximatic/models/stock/stock_picking_batch.py
+++ fleximatic/models/stock/stock_picking_batch.py
@@ -6,10 +6,14 @@ import logging
 _logger = logging.getLogger(__name__)
 
 class fleximaticstockbatch(models.Model):
-    
     _inherit = 'stock.picking.batch'
     x_purchase = fields.Many2one('purchase.order',string='Logistics purchase')
-    #@api.onchange('picking_ids')
-    #def _onchange_pickings(self):
-    
+    total_sales = fields.Float('Total ventas', compute='_compute_total_sales')
+    @api.onchange('picking_ids')
+    def _onchange_pickings(self):
+        if len(self.picking_ids) == len(self.picking_ids.filtered(lambda x:self.x_purchase == x.x_logistics ) ) :
+            pass
+        else:
+            pass
+
             
