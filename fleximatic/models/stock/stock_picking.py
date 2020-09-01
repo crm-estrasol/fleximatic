ded9426d825b07dbe71ae13477ec61267eed7165
Kevin-lCampo-Estrasol <kcampo@estrasol.com.mx>
2020-09-01 11:22:11 -0500

    s_1_1

--- fleximatic/models/stock/stock_picking.py
+++ fleximatic/models/stock/stock_picking.py
@@ -25,6 +25,7 @@ class fleximaticstock(models.Model):
         for record in self:
             record['x_freight'] = record.x_freight_cost / (record.x_total and record.x_total or 1)
     def write(self, vals): 
+        self.ensure_one()
         items = self.env['stock.picking.batch'].search([('picking_ids','=',self.id )])
         for item in items:
             if self.x_logistics.id != item.x_purchase.id:
