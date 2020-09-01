2e12aced51c9f24803bebfbdca7bb9809f2299b1
Kevin-lCampo-Estrasol <kcampo@estrasol.com.mx>
2020-09-01 12:31:22 -0500

    s_1_1

--- fleximatic/models/stock/stock_picking_batch.py
+++ fleximatic/models/stock/stock_picking_batch.py
@@ -21,7 +21,7 @@ class fleximaticstockbatch(models.Model):
             ids = self.picking_ids.filtered(lambda x:self.x_purchase == x.x_logistics ).mapped('id')
             self.picking_ids = [( 6, 0, [x for x in ids] ) ]
         else: 
-            ids = self.picking_ids.filtered(lambda x:self.x_purchase == False ).mapped('id')
+            ids = self.picking_ids.filtered(lambda x: x.x_logistics == False ).mapped('id')
             self.picking_ids = [( 6, 0, [x for x in ids] ) ]
                 
     @api.onchange('x_purchase')  
