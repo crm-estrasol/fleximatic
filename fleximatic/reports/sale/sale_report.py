82a5cd07c299c93ca778c42ee1f220dd31556ec6
Kevin-lCampo-Estrasol <kcampo@estrasol.com.mx>
2020-10-05 16:00:27 -0500

    reporte

--- fleximatic/reports/sale/sale_report.py
+++ fleximatic/reports/sale/sale_report.py
@@ -7,10 +7,12 @@ class SaleReportFlexi(models.Model):
     _inherit = 'sale.report'
     #invoice_line_id = fields.Many2one('account.move.line', 'Invoice reference', readonly=True)
     acount_move = fields.Char(string='Facturas')
+    invoice_date = fields.Date(string="Fecha factura")
     def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
         #fields['invoice_line_id'] = ', soli.invoice_line_id as invoice_line_id'
         fields['acount_move'] = ', aci.name as acount_move'
-        groupby += ', aci.name'
+        fields['invoice_date'] = ', aci.invoice_date as invoice_date'
+        groupby += ', aci.name , aci.invoice_date'
         from_clause  +=  """
                             left join sale_order_line_invoice_rel soli on (l.id = soli.order_line_id)
                             left join account_move_line acil on (acil.id = soli.invoice_line_id )
