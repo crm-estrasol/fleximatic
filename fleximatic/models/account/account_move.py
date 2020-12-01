38f57fb4b8ad9a45f41fcd9c97a00ee02cf147b9
Kevin-lCampo-Estrasol <kcampo@estrasol.com.mx>
2020-11-30 18:05:21 -0600

    adenda

--- fleximatic/models/account/account_move.py
+++ fleximatic/models/account/account_move.py
@@ -8,8 +8,8 @@ class fleximatiAccountMove(models.Model):
     def adenda_walmart(self,actual_inv):
         actual_inv = actual_inv
         segments = []
-        numero_control = "555"
-        fecha_s1 = ""
-        hora_s1 = ""
-        segments.append("""UNB+UNOA:2+MXG1390:ZZ+925485MX00:8+%s:%s+%s'""" % (fecha_s1,hora_s1,numero_control))
+        control = "555"
+        date_s1 = actual_inv.invoice_date.strftime("%Y")[-2:]+actual_inv.invoice_date.strftime("%m")+actual_inv.invoice_date.strftime("%d")
+        hour_s1 = fields.Datetime.today().strftime("%H%S")
+        segments.append("""UNB+UNOA:2+MXG1390:ZZ+925485MX00:8+%s:%s+%s'""" % (date_s1,hour_s1,control))
         return "".join(segments)
