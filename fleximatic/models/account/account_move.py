1cd1f00353c7912ff41b12466e9f41a72dc3301f
Kevin-lCampo-Estrasol <kcampo@estrasol.com.mx>
2020-12-02 13:22:23 -0600

    new adenda

--- fleximatic/models/account/account_move.py
+++ fleximatic/models/account/account_move.py
@@ -5,8 +5,14 @@ from odoo import api, fields, models, SUPERUSER_ID, _
 class fleximatiAccountMove(models.Model):
     
     _inherit = 'account.move'
-    num_order = fields.Char('Numero de orden de compra')
+   
     addenda_verify = fields.Char(related="partner_id.l10n_mx_edi_addenda.name")
+    total_letter =  fields.Char('Total en numero')
+    num_order = fields.Char('Numero de orden de compra')
+    date_order = fields.Date('Fecha orden')
+    buyEan_code = fields.Char('Código EAN comprador')
+    number_supplier = fields.Char('Numero proveedor 9 digitos',size=9)
+
     def adenda_walmart(self,actual_inv):
         actual_inv = actual_inv
         #Vars needed
@@ -16,16 +22,18 @@ class fleximatiAccountMove(models.Model):
         invoiceId_s3 = str(actual_inv.id)
         date_s4 = actual_inv.invoice_date.strftime("%Y")+actual_inv.invoice_date.strftime("%m")+actual_inv.invoice_date.strftime("%d")
         totalLetter_s5 = "PENDIENTE PESOS MX"
-        orderBuy_s5 = "Manual"
-        orderDate_s6 = "Manual"
-        invoiceSerie_s7  = actual_inv.name
-        buyEanCode_s9 = "Manual"
-        numberSupplier9s_s13 = "Manual"
+        orderBuy_s6 = "Manual"
+        orderDate_s7 = "Manual"
+        invoiceSerie_s8  = actual_inv.name
+        buyEanCode_s10 = "Manual"
+        #Global
+        sellEanCode_s12 ="7504023427002" 
+        numberSupplier9s_s14 = "Manual"
         if actual_inv.invoice_payment_term_id:
             days = sum([ line.days for line in  actual_inv.invoice_payment_term_id.line_ids]) 
         else:
             days = 0
-        creditDays_s16 = days
+        creditDays_s17 = days
 
         segments = [
         """UNB+UNOA:2+MXG1390:ZZ+925485MX00:8+%s:%s+%s'""" % (date_s1,hour_s1,control)  , 
@@ -33,17 +41,17 @@ class fleximatiAccountMove(models.Model):
         """BGM+380+%s+9'""" % (invoiceId_s3), 
         """DTM+137:%s:102'""" % (date_s4) ,
         """FTX+ZZZ+++%s+ES'""" % (totalLetter_s5),
-        """RFF+ON:%s'""" % (orderBuy_s5), 
-        """DTM+171:%s:102'""" % (orderDate_s6), 
-        """RFF+BT:%s'""" % (invoiceSerie_s7) ,
+        """RFF+ON:%s'""" % (orderBuy_s6), 
+        """DTM+171:%s:102'""" % (orderDate_s7), 
+        """RFF+BT:%s'""" % (invoiceSerie_s8) ,
         """RFF+ATZ:0'""",
-        """NAD+BY+%s::9++NUEVA WAL MART DE MEXICO S DE RL DE:CV+NEXTENGO # 78:COL SANTA CRUZ ACAYUCAN+CIUDAD DE MEXICO+DF+02770'""" % (buyEanCode_s9),
+        """NAD+BY+%s::9++NUEVA WAL MART DE MEXICO S DE RL DE:CV+NEXTENGO # 78:COL SANTA CRUZ ACAYUCAN+CIUDAD DE MEXICO+DF+02770'""" % (buyEanCode_s10),
         """RFF+GN:NWM9709244W4'""",
-        """NAD+SU+7504023427002::9++FLEXIMATIC SA DE CV+CAMINO REAL DE COLIMA:901 14+SANTA ANITA+JALISCO+45645'""",
+        """NAD+SU+%s::9++FLEXIMATIC SA DE CV+CAMINO REAL DE COLIMA:901 14+SANTA ANITA+JALISCO+45645'""" % (sellEanCode_s12),
         """RFF+GN:FLE980113E95'""",
-        """RFF+IA:%s'""" %( numberSupplier9s_s13),
+        """RFF+IA:%s'""" %( numberSupplier9s_s14),
         """NAD+ST+7507003116583::9++WALMART DC+CARRETERA EL SALTO NO 420:LAS ALAMEDAS+TLAJOMULCO DE ZUNIGA+JALISCO+45679'""",
         """CUX+2:MXN:4'""",
-        """PAT+1++5:3:D:%s'""" % (creditDays_s16),
+        """PAT+1++5:3:D:%s'""" % (creditDays_s17),
         ]
         return "".join(segments)
