432e6a4fe07dab06fe48c31b548b0d9bc7bc98f3
Kevin-lCampo-Estrasol <kcampo@estrasol.com.mx>
2020-12-04 13:59:57 -0600

    adenda

--- fleximatic/models/account/account_move copy.py
+++ fleximatic/models/account/account_move copy.py
@@ -50,3 +50,241 @@ class fleximatiAccountMove(models.Model):
         #RFC fleximatic  
         segments.append("""RFF+IA:%s'""")
         return "".join(segments)
+
+
+        # Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+           
+def literal_number(num):
+    lista_centana = ["",("CIEN","CIENTO "),"DOSCIENTOS ","TRESCIENTOS","CUATROCIENTOS","QUINIENTOS","SEISCIENTOS","SETECIENTOS","OCHOCIENTOS","NOVECIENTOS"]
+    lista_decena = ["",("DIEZ","ONCE",
+                        "DOCE","TRECE","CATORCE",
+                        "QUINCE","DIECISEIS",
+                        "DIECISIETE","DIECIOCHO","DIECINUEVE"), 
+                        ("VEINTE","VEINTI"),
+                        ("TREINTA","TREINTA Y "),
+                        ("CUARENTA" , "CUARENTA Y "), 
+                        ("CINCUENTA" , "CINCUENTA Y "),
+                        ("SESENTA" , "SESENTA Y "),
+                        ("SETENTA" , "SETENTA Y "),
+                        ("OCHENTA" , "OCHENTA Y "),
+    					("NOVENTA" , "NOVENTA Y ")
+    				]
+    lista_unidad = ["",("UN" , "UNO"),"DOS","TRES","CUATRO","CINCO","SEIS","SIETE","OCHO","NUEVE"]
+    indicador = [("",""),("MIL","MIL"),("MILLON","MILLONES"),("MIL","MIL"),("BILLON","BILLONES")]
+    def numero_to_letras(numero):
+        arrays = last(numero)
+        word = ""
+        index = 0
+        print(arrays)
+        lenarray = len(arrays)
+        for x in arrays:
+            index+=1
+            x = "".join(('00' if len(x) == 1 else '0' if  len(x) == 2 else "", x))
+            
+            value = indicador[index][0] if x == '001' and index != lenarray else centenas(x) 
+            word +=value +" " +(str(indicador[index][1]+"0") if index != lenarray else '')   
+        return word    
+    def last(a):
+        ln = len(a) 
+        if ln == 5 :
+            return [a[:2]] + last(a[-3:]) 
+        if ln == 4:
+            return [a[:1]] + last(a[-3:]) 
+        if  ln <= 3:
+            return [a]
+        return [a[-3:]] + last(a[:-3])  
+    def centenas(number):
+        if len(number) == 3:
+            dec = ""
+            cent = ""
+            if number[1]  == "0" and number[2]  == "0" and number[0] == "1":
+                return lista_centana[ int(number[0]) ][0]
+            elif number[0] == "1" :
+                 cent = lista_centana[ int(number[0]) ][1]            
+            else:
+                cent = lista_centana[ int(number[0]) ]  
+            if  number[1] == "1":
+                print(str(int(number[2])))
+                cent +=" "+lista_decena[ int(number[1]) ][int(number[2])]
+                return cent
+            elif number[1] != "0":
+                cent +=" "+lista_decena[ int(number[1]) ][ 0 if int(number[2]) == 0 else 1 ]
+            if number[1] == "2"  and  number[2] == "1":
+                dec = lista_unidad[ int(number[2]) ][0]
+            elif number[2] == "1":
+                dec = lista_unidad[ int(number[2]) ][1]
+            else:
+                 dec =(" " if number[1] == "0" else "") +  lista_unidad[ int(number[2]) ]
+            
+            return cent+dec
+    return numero_to_letras(num)
+print(literal_number("150321"))
+ 
+def numero_to_letras(numero):
+    indicador = [('', ''), ('MIL', 'MIL'), ('MILLON', 'MILLONES'),
+                 ('MIL', 'MIL'), ('BILLON', 'BILLONES')]
+    entero = int(numero)
+    decimal = int(round((numero - entero) * 100))
+
+    # print 'decimal : ',decimal
+
+    contador = 0
+    numero_letras = ''
+    while entero > 0:
+        a = entero % 1000
+        if contador == 0:
+            en_letras = convierte_cifra(a, 1).strip()
+        else:
+            en_letras = convierte_cifra(a, 0).strip()
+        if a == 0:
+            numero_letras = en_letras + ' ' + numero_letras
+        elif a == 1:
+            if contador in (1, 3):
+                numero_letras = indicador[contador][0] + ' ' \
+                    + numero_letras
+            else:
+                numero_letras = en_letras + ' ' \
+                    + indicador[contador][0] + ' ' + numero_letras
+        else:
+            numero_letras = en_letras + ' ' + indicador[contador][1] \
+                + ' ' + numero_letras
+        numero_letras = numero_letras.strip()
+        contador = contador + 1
+        entero = int(entero / 1000)
+    numero_letras = numero_letras + ' con ' + str(decimal) + '/100'
+    print( numero )
+    print( numero_letras)
+
+
+def numero_to_letras(numero):
+    indicador = [('', ''), ('MIL', 'MIL'), ('MILLON', 'MILLONES'),
+                 ('MIL', 'MIL'), ('BILLON', 'BILLONES')]
+    entero = int(numero)
+    decimal = int(round((numero - entero) * 100))
+
+    # print 'decimal : ',decimal
+
+    contador = 0
+    numero_letras = ''
+    while entero > 0:
+        a = entero % 1000
+        if contador == 0:
+            en_letras = convierte_cifra(a, 1).strip()
+        else:
+            en_letras = convierte_cifra(a, 0).strip()
+        
+        if a == 0:
+            numero_letras = en_letras + ' ' + numero_letras
+        elif a == 1:
+            if contador in (1, 3):
+                numero_letras = indicador[contador][0] + ' ' \
+                    + numero_letras
+                    
+            else:
+                numero_letras = en_letras + ' ' \
+                    + indicador[contador][0] + ' ' + numero_letras
+                
+        else:
+            numero_letras = en_letras + ' ' + indicador[contador][1] \
+                + ' ' + numero_letras
+            print(numero_letras)
+        numero_letras = numero_letras.strip()
+        contador = contador + 1
+        entero = int(entero / 1000)
+    numero_letras = numero_letras + ' con ' + str(decimal) + '/100'
+    print( numero )
+    print( numero_letras)
+
+
+def convierte_cifra(numero, sw):
+    lista_centana = [
+        '',
+        ('CIEN', 'CIENTO'),
+        'DOSCIENTOS',
+        'TRESCIENTOS',
+        'CUATROCIENTOS',
+        'QUINIENTOS',
+        'SEISCIENTOS',
+        'SETECIENTOS',
+        'OCHOCIENTOS',
+        'NOVECIENTOS',
+        ]
+    lista_decena = [
+        '',
+        (
+            'DIEZ',
+            'ONCE',
+            'DOCE',
+            'TRECE',
+            'CATORCE',
+            'QUINCE',
+            'DIECISEIS',
+            'DIECISIETE',
+            'DIECIOCHO',
+            'DIECINUEVE',
+            ),
+        ('VEINTE', 'VEINTI'),
+        ('TREINTA', 'TREINTA Y '),
+        ('CUARENTA', 'CUARENTA Y '),
+        ('CINCUENTA', 'CINCUENTA Y '),
+        ('SESENTA', 'SESENTA Y '),
+        ('SETENTA', 'SETENTA Y '),
+        ('OCHENTA', 'OCHENTA Y '),
+        ('NOVENTA', 'NOVENTA Y '),
+        ]
+    lista_unidad = [
+        '',
+        ('UN', 'UNO'),
+        'DOS',
+        'TRES',
+        'CUATRO',
+        'CINCO',
+        'SEIS',
+        'SIETE',
+        'OCHO',
+        'NUEVE',
+        ]
+    centena = int(numero / 100)
+    decena = int((numero - centena * 100) / 10)
+    unidad = int(numero - (centena * 100 + decena * 10))
+
+    # print "centena: ",centena, "decena: ",decena,'unidad: ',unidad
+
+    texto_centena = ''
+    texto_decena = ''
+    texto_unidad = ''
+
+    # Validad las centenas
+
+    texto_centena = lista_centana[centena]
+    if centena == 1:
+        if decena + unidad != 0:
+            texto_centena = texto_centena[1]
+        else:
+            texto_centena = texto_centena[0]
+
+    # Valida las decenas
+
+    texto_decena = lista_decena[decena]
+    if decena == 1:
+        texto_decena = texto_decena[unidad]
+    elif decena > 1:
+        if unidad != 0:
+            texto_decena = texto_decena[1]
+        else:
+            texto_decena = texto_decena[0]
+
+     # Validar las unidades
+     # print "texto_unidad: ",texto_unidad
+
+    if decena != 1:
+        texto_unidad = lista_unidad[unidad]
+    
+        if unidad == 1 :
+            texto_unidad = texto_unidad[sw]
+            return '%s %s%s' % (texto_centena, texto_decena, texto_unidad)
+    if decena == 2 :
+        return '%s %s%s' % (texto_centena, texto_decena, texto_unidad)
+    return '%s %s %s' % (texto_centena, texto_decena, texto_unidad)
+print(numero_to_letras(120))
--- fleximatic/models/account/account_move.py
+++ fleximatic/models/account/account_move.py
@@ -21,7 +21,7 @@ class fleximatiAccountMove(models.Model):
         hour_s1 = fields.Datetime.today().strftime("%H%S")
         invoiceId_s3 = str(actual_inv.id)
         date_s4 = actual_inv.invoice_date.strftime("%Y")+actual_inv.invoice_date.strftime("%m")+actual_inv.invoice_date.strftime("%d")
-        totalLetter_s5 = "PENDIENTE PESOS MX"
+        totalLetter_s5 = str(self.numero_to_letras(actual_inv.amount_total) )
         orderBuy_s6 = "Manual"
         orderDate_s7 = "Manual"
         invoiceSerie_s8  = actual_inv.name
@@ -54,4 +54,146 @@ class fleximatiAccountMove(models.Model):
         """CUX+2:MXN:4'""",
         """PAT+1++5:3:D:%s'""" % (creditDays_s17),
         ]
+        segments_elements = [
+               "".join( ["""LIN+1++%s:SRV::9'""" % (prod.product_id.barcode),
+               """PIA+1+%s:IN'""" % (prod.product_id.default_code),
+               """IMD+F++:::%s %s::ES'""" % (prod.description_sale,prod.description_sale),
+               """QTY+47:%s:EA'""" % (str(prod.price_tax+prod.price_subtotal )),
+               """MOA+203:%s'""" % (str(prod.price_subtotal )),
+               """PRI+AAA:%s::::EA'""" % ( str(prod.price_unit ) ),
+               """TAX+7+VAT+++:::%s+B'""" % ( "".join( [x.ammount for x in  prod.tax_ids] )  ),
+               """MOA+124:%s'""" % (str(prod.price_tax ))] )
+                                                    for prod in actual_inv.invoice_line_ids] 
+        segments.append(segments_elements)
         return "".join(segments)
+    def numero_to_letras(self,numero):
+        indicador = [('', ''), ('MIL', 'MIL'), ('MILLON', 'MILLONES'),
+                    ('MIL', 'MIL'), ('BILLON', 'BILLONES')]
+        entero = int(numero)
+        decimal = int(round((numero - entero) * 100))
+
+        # print 'decimal : ',decimal
+
+        contador = 0
+        numero_letras = ''
+        while entero > 0:
+            a = entero % 1000
+            if contador == 0:
+                en_letras = self.convierte_cifra(a, 1).strip()
+            else:
+                en_letras = self.convierte_cifra(a, 0).strip()
+            
+            if a == 0:
+                numero_letras = en_letras + ' ' + numero_letras
+            elif a == 1:
+                if contador in (1, 3):
+                    numero_letras = indicador[contador][0] + ' ' \
+                        + numero_letras
+                        
+                else:
+                    numero_letras = en_letras + ' ' \
+                        + indicador[contador][0] + ' ' + numero_letras
+                    
+            else:
+                numero_letras = en_letras + ' ' + indicador[contador][1] \
+                    + ' ' + numero_letras
+                print(numero_letras)
+            numero_letras = numero_letras.strip()
+            contador = contador + 1
+            entero = int(entero / 1000)
+        numero_letras = numero_letras + ' con ' + str(decimal) + '/100'
+        print( numero )
+        print( numero_letras)
+
+
+    def convierte_cifra(self,numero, sw):
+        lista_centana = [
+            '',
+            ('CIEN', 'CIENTO'),
+            'DOSCIENTOS',
+            'TRESCIENTOS',
+            'CUATROCIENTOS',
+            'QUINIENTOS',
+            'SEISCIENTOS',
+            'SETECIENTOS',
+            'OCHOCIENTOS',
+            'NOVECIENTOS',
+            ]
+        lista_decena = [
+            '',
+            (
+                'DIEZ',
+                'ONCE',
+                'DOCE',
+                'TRECE',
+                'CATORCE',
+                'QUINCE',
+                'DIECISEIS',
+                'DIECISIETE',
+                'DIECIOCHO',
+                'DIECINUEVE',
+                ),
+            ('VEINTE', 'VEINTI'),
+            ('TREINTA', 'TREINTA Y '),
+            ('CUARENTA', 'CUARENTA Y '),
+            ('CINCUENTA', 'CINCUENTA Y '),
+            ('SESENTA', 'SESENTA Y '),
+            ('SETENTA', 'SETENTA Y '),
+            ('OCHENTA', 'OCHENTA Y '),
+            ('NOVENTA', 'NOVENTA Y '),
+            ]
+        lista_unidad = [
+            '',
+            ('UN', 'UNO'),
+            'DOS',
+            'TRES',
+            'CUATRO',
+            'CINCO',
+            'SEIS',
+            'SIETE',
+            'OCHO',
+            'NUEVE',
+            ]
+        centena = int(numero / 100)
+        decena = int((numero - centena * 100) / 10)
+        unidad = int(numero - (centena * 100 + decena * 10))
+
+        # print "centena: ",centena, "decena: ",decena,'unidad: ',unidad
+
+        texto_centena = ''
+        texto_decena = ''
+        texto_unidad = ''
+
+        # Validad las centenas
+
+        texto_centena = lista_centana[centena]
+        if centena == 1:
+            if decena + unidad != 0:
+                texto_centena = texto_centena[1]
+            else:
+                texto_centena = texto_centena[0]
+
+        # Valida las decenas
+
+        texto_decena = lista_decena[decena]
+        if decena == 1:
+            texto_decena = texto_decena[unidad]
+        elif decena > 1:
+            if unidad != 0:
+                texto_decena = texto_decena[1]
+            else:
+                texto_decena = texto_decena[0]
+
+        # Validar las unidades
+        # print "texto_unidad: ",texto_unidad
+
+        if decena != 1:
+            texto_unidad = lista_unidad[unidad]
+        
+            if unidad == 1 :
+                texto_unidad = texto_unidad[sw]
+                return '%s %s%s' % (texto_centena, texto_decena, texto_unidad)
+        if decena == 2 :
+            return '%s %s%s' % (texto_centena, texto_decena, texto_unidad)
+        return '%s %s %s' % (texto_centena, texto_decena, texto_unidad)
+  
